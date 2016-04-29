e68707475656eec21fddeabd7c3bec1e21f23ea7
Clifton Best <clifton.best@me.com>
2016-04-28 20:57:28 -0400

    1. Modified zipf_work() function to produce 3 types of frequency spectrum plots. 2. Added vocabulary growth plot function after for loop.

--- zipfrFreq.R
+++ zipfrFreq.R
@@ -10,26 +10,28 @@ this.spc <- text2spc.fnc(text)
 summary(this.spc)
 #plot histogram of frequency classes
 plot(this.spc)
+#plot first 50 elements
+plot(this.spc, log="x")
 #plot frequency spectrum
 with(this.spc, plot(m, Vm, main="Frequency Spectrum"))
 #create growth object
-this_growth<- growth.fnc(text)
-summary(this_growth)
+#this_growth<- growth.fnc(text)
+#summary(this_growth)
 #create vgc from growth
-this_vgc <- growth2vgc.fnc(this_growth)
-summary(this_vgc)
+#this_vgc <- growth2vgc.fnc(this_growth)
+#summary(this_vgc)
 #plot vocabulary growth
-plot(this_vgc, add.m=1)
+#plot(this_vgc, add.m=1)
 #plot interpolated vocabulary growth
 #this_bin_vgc <- vgc.interp(this.spc, N(this_vgc), m.max=1)
 #estimating V and other 
-this.fzm <- lnre("fzm", this.spc, exact=FALSE)
-summary(this.fzm)
+#this.fzm <- lnre("fzm", this.spc, exact=FALSE)
+#summary(this.fzm)
 #this.fzm.spc <- lnre.spc(this.fzm, N(this.fzm))
 #plot(this.spc, this.fzm.spc, legend=c("observed", "fZM"))
-this_zm <- lnre("zm", this.spc)
-this.zm.spc <- lnre.spc(this_zm, 10e6)
-this.zm.spc
+#this_zm <- lnre("zm", this.spc)
+#this.zm.spc <- lnre.spc(this_zm, 10e6)
+#this.zm.spc
 
 
 
